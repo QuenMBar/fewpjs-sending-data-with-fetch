2fde30d20346372235eca0cc85840d76d03f4aae
QuenMBar <Qbeast360@gmail.com>
2021-03-01 11:09:28 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+let idNum = document.getElementById("idNum");
+let form = document.getElementById("idNum");
+/**
+ * @param {string} name
+ * @param {string} email
+ */
+function submitData(name, email) {
+    // method: "POST" is missing from the object below
+    let formData = {
+        name: name,
+        email: email,
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object.id);
+            idNum.innerHTML = object.id;
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        });
+}
